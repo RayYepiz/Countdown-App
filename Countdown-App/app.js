e68d4909236fde475030421a282cde3e9e306364
Vianey Valencia <vianey@Vianeys-MacBook-Pro.local>
2022-07-08 12:42:10 -0700

    adding javascript

---
+++ Countdown-App/app.js
@@ -0,0 +1,10 @@
+const bday = '6 Aug 2022';
+
+function countDown(){
+    const targetDate = new Date(bday);
+    const currentDate = new Date();
+    const timeRemaining = targetDate - currentDate;
+    const months = 
+}
+
+countDown();
